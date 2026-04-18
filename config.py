668c1759a304e6eb8993fcf9102d54f6dd499691
suxas <1764721498@qq.com>
2026-04-18 13:22:02 +0800

    修改cifar对应的任务模型为VGG

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     # --- 环境与联邦学习设置 ---
     num_users = 30
     num_edge_servers = 3
-    num_global_rounds = 500
+    num_global_rounds = 1000
     num_local_epochs = 1
     batch_size = 64         # 客户端本地训练batchsize
     lr = 0.005              # 客户端本地训练学习率
@@ -30,7 +30,7 @@ class Config:
     cycles_per_sample = 2e4
 
     # 掉队率与物理死线
-    target_straggler_rate = 0.9
+    target_straggler_rate = 0.7
     t_deadline = 99999.0
     noise_scale = 0.001
 
